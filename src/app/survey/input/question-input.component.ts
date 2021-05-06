10eabdd4ddaad8df832bda126a7967d33f655b9f
DanieleSalaris <44474878+DanieleSalaris@users.noreply.github.com>
2021-05-06 18:35:27 +0200

    question input

--- src/app/survey/input/question-input.component.ts
+++ src/app/survey/input/question-input.component.ts
@@ -1,14 +1,45 @@
 import {Component, Input} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {max} from 'rxjs/operators';
 
 @Component({
   selector: 'app-question-input',
   templateUrl: './question-input.component.html',
   styleUrls: ['./question-input.component.css', '../question/question.component.css']
 }) export class QuestionInputComponent {
-  @Input() type: string;
   @Input() label: string;
-  @Input() minLength: number;
-  @Input() maxLength: number;
 
-  constructor() {}
+  @Input() set textConfigValues(value) {
+    const {
+      type,
+      minLength,
+      maxLength,
+    } = value;
+
+    if (this.type !== type || this.minLength !== minLength || this.maxLength !== maxLength) { // changed values
+      this.formGroup = this.fb.group({
+        value: this.fb.control([[''], [Validators.required, Validators.minLength(minLength), Validators.maxLength(maxLength)]])
+      });
+    }
+
+    this.type = type;
+    this.minLength = minLength;
+    this.maxLength = maxLength;
+  }
+
+  get textConfigValues() {
+    return {
+      type: this.type,
+      minLength: this.minLength,
+      maxLength: this.maxLength
+    };
+  }
+
+  private type: string;
+  private minLength: number;
+  private maxLength: number;
+
+  formGroup = this.fb.group({});
+
+  constructor(private fb: FormBuilder) {}
 }
