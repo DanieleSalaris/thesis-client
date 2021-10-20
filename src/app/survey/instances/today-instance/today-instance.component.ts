149693908427868e0317a835b9644490fee6df5c
DanieleSalaris <44474878+DanieleSalaris@users.noreply.github.com>
2021-10-20 19:40:48 +0200

    fixes

--- src/app/survey/instances/today-instance/today-instance.component.ts
+++ src/app/survey/instances/today-instance/today-instance.component.ts
@@ -2,23 +2,33 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SurveyService} from '@src/app/survey/survey.service';
 import {Subscription} from 'rxjs';
 import {Router} from '@angular/router';
-import {switchMap} from 'rxjs/operators';
+import { tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-today-instance',
-  template: ''
+  templateUrl: './today-instance.component.html',
+  styleUrls: ['./today-instance.component.css']
 }) export class TodayInstanceComponent implements OnInit, OnDestroy {
   todayInstanceSubscription: Subscription;
+  instanceId;
 
   constructor(private surveyService: SurveyService, private router: Router) {}
 
   ngOnInit() {
     this.todayInstanceSubscription = this.surveyService.getTodayInstance().pipe(
-      switchMap(instance => this.router.navigate(['instance', instance._id]))
+      tap(instance => this.instanceId = instance._id),
+      // switchMap(instance => this.router.navigate(['instance', instance._id]))
     ).subscribe();
   }
 
   ngOnDestroy() {
     this.todayInstanceSubscription?.unsubscribe();
+  }
+
+  navigateToQuestion() {
+    if (!this.instanceId) {
+      return;
+    }
+    this.router.navigate(['instance', this.instanceId]).catch();
   }
 }
