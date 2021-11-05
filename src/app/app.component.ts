a46068b64e4ff322fde4cb7e8319443e81b37a1d
daniele <dnl.salaris@gmail.com>
2021-11-05 12:40:23 +0100

    notifications working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { LocalNotifications } from '@nativescript/local-notifications';
+import {UtilityService} from '@src/app/utility/utility.service';
 
 @Component({
   selector: 'app-root',
@@ -7,41 +8,15 @@ import { LocalNotifications } from '@nativescript/local-notifications';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {
+  constructor(
+      private utilityService: UtilityService
+  ) {
     LocalNotifications.addOnMessageReceivedCallback(notificationData => {
-      console.log("Notification received: " + JSON.stringify(notificationData));
+      console.log('Notification received: ' + JSON.stringify(notificationData));
     });
   }
 
   ngOnInit() {
-    LocalNotifications.schedule(
-        [{
-          id: 5,
-          thumbnail: true,
-          title: 'Richard wants your input',
-          body: '"Hey man, what do you think of the new design?" (swipe down to reply, or tap to open the app)',
-          forceShowWhenInForeground: true,
-          at: new Date(new Date().getTime() + 120 * 1000),
-          actions: [
-            {
-              id: 'input-richard',
-              type: 'input',
-              title: 'Tap here to reply',
-              placeholder: 'Type to reply..',
-              submitLabel: 'Reply',
-              launch: true,
-              editable: true,
-              // choices: ["Red", "Yellow", "Green"] // TODO Android only, but yet to see it in action
-            }
-          ]
-        }])
-        .then(() => {
-          alert({
-            title: 'Notification scheduled',
-            message: 'ID: 5',
-            okButtonText: 'OK, thanks'
-          });
-        })
-        .catch(error => console.log('doScheduleId5WithInput error: ' + error));
+    this.utilityService.scheduleSurveyNotifications();
   }
 }
--- src/app/utility/utility.service.tns.ts
+++ src/app/utility/utility.service.tns.ts
@@ -1,7 +1,10 @@
 import {Injectable} from '@angular/core';
 import {isAndroid, isIOS} from '@nativescript/core';
+import {LocalNotifications} from '@nativescript/local-notifications';
 import * as utils from '@nativescript/core/utils/utils';
 declare const UIApplication;
+
+import {getDate, getMonth, getYear, isBefore, parse, addDays, setHours, setMinutes} from 'date-fns';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +21,60 @@ export class UtilityService {
     if (isIOS) {
       UIApplication.sharedApplication.keyWindow.endEditing(true);
     }
+  }
+
+  scheduleSurveyNotifications() {
+    const notifications = this.generateNotifications();
+
+    LocalNotifications.schedule(notifications).catch();
+  }
+
+  private generateNotifications(numberOfDays = 10) {
+    // message that appear on notification
+    const message = {
+      title: 'Hi!',
+      body: 'Remember to compile the survey'
+    };
+
+    // time format that appear on notification id to avoid duplication
+    const timeFormat = 'yyyy-MM-d HH:mm';
+
+    // hours in which notifications appears
+    const notificationHours = ['10:00', '18:00'];
+
+    // array of notifications to return
+    const notifications = [];
+    const now = new Date();
+
+    for (let i = 0; i < numberOfDays; i++) {
+      // on each iteration take the next day
+      const date = addDays(now, i);
+
+      // extract year month and day
+      const year = getYear(date);
+      const month = getMonth(date) + 1;
+      const day = getDate(date);
+
+      // format times
+      const notificationsTimes = notificationHours.map(hour => `${year}-${month}-${day} ${hour}`);
+
+      notificationsTimes.forEach(nt => {
+        const ntDate = parse(nt, timeFormat, new Date());
+
+        // check that the time is not in the past
+        if (i === 0 && isBefore(ntDate, now)) {
+          return;
+        }
+
+        // push notification on return array
+        notifications.push({
+          id: nt,
+          at: ntDate,
+          ...message
+        });
+      });
+    }
+
+    return notifications;
   }
 }
